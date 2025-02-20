d5fd2075d4bb9b2a8e5a990cc4710f68636ba5f6
ToprakArslann <toprakarslan0139@gmail.com>
2025-02-20 11:38:43 +0300

    Cmake Build Files

---
+++ build/CMakeFiles/dithering-app.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dithering-app.dir/link.d"
+  "CMakeFiles/dithering-app.dir/src/main.cpp.o"
+  "CMakeFiles/dithering-app.dir/src/main.cpp.o.d"
+  "dithering-app"
+  "dithering-app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dithering-app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
